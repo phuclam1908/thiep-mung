15a40579a87988070edf20cef5948b959c5a8e8d
phuclam1908 <93136613+phuclam1908@users.noreply.github.com>
2022-05-25 21:26:49 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = '';
+const giftUrl = 'https://www.facebook.com/nguyenle.phuclam.1908';
+const eventName = 'Happy Birthday';
+const titleCard = 'Small present';
+const contentCard = 'Happy Birthday Make A Wish. Dear brother, hope you receive everything you wish for this year. You deserve the best! Thanks for being such a great brother.💘';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
